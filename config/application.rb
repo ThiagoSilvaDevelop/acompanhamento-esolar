6f505c4ac3c7ee4001ca34dc151f29073dd6c14c
Thiago Araújo <thiagosilva.java@gmail.com>
2020-01-02 20:15:06 -0300

    config assets

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module DeliciasDaMoni
   class Application < Rails::Application
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
